70670eba68f84eda7857dfaf5d561459a8316b74
johnrachwan123 <johnrachwan@gmail.com>
2025-03-19 06:24:33 +0000

    feat: enable combination of torch_compile and hqq_diffusers

--- src/pruna/engine/load.py
+++ src/pruna/engine/load.py
@@ -294,22 +294,21 @@ def load_hqq_diffusers(path: str, **kwargs) -> Any:
         model_index = load_json_config(path, "model_index.json")
 
         cls = getattr(diffusers, model_index["_class_name"])
-        # we need to load the original model pipeline, and
-        # then replace the unet/transformer with the one from model_path.
-        loaded_transformer = AutoHQQHFDiffusersModel.from_quantized(path + "/transformer_quantized")
-        if "transformer" in model_index:
-            # transformers discards kwargs automatically, no need for filtering
-            model = cls.from_pretrained(path, transformer=None, **kwargs)
-            model.transformer = loaded_transformer
-        elif "unet" in model_index:
-            # transformers discards kwargs automatically, no need for filtering
-            model = cls.from_pretrained(path, unet=None, **kwargs)
-            model.unet = loaded_transformer
-            for layer in model.unet.up_blocks:
-                if layer.upsamplers is not None:
-                    layer.upsamplers[0].name = "conv"
+        if os.path.exists(os.path.join(path, "backbone_quantized")):
+            loaded_backbone = AutoHQQHFDiffusersModel.from_quantized(os.path.join(path, "backbone_quantized"))
         else:
-            raise ValueError("No transformer or unet found in model_index.json")
+            loaded_backbone = None
+        if loaded_backbone is not None:
+            if "transformer" in model_index:
+                model = cls.from_pretrained(path, transformer=None, **kwargs)
+                model.transformer = loaded_backbone
+            elif "unet" in model_index:
+                model = cls.from_pretrained(path, unet=loaded_backbone, **kwargs)
+                for layer in model.unet.up_blocks:
+                    if layer.upsamplers is not None:
+                        layer.upsamplers[0].name = "conv"
+        else:
+            model = AutoHQQHFDiffusersModel.from_quantized(path)
     return model
 
 
