3463be9f6cf69b3db77d4706195f2a421977c39d
llcnt <louis.leconte@ens-paris-saclay.fr>
2025-04-07 11:01:59 +0000

    feat: add gptq compatibility

--- src/pruna/algorithms/compilation/torch_compile.py
+++ src/pruna/algorithms/compilation/torch_compile.py
@@ -49,7 +49,7 @@ class TorchCompileCompiler(PrunaCompiler):
     run_on_cuda = True
     dataset_required = False
     compatible_algorithms = dict(
-        quantizer=["half", "hqq_diffusers", "diffusers_int8"],
+        quantizer=["half", "hqq_diffusers", "diffusers_int8", "gptq"],
         cacher=["deepcache"],
     )
 
