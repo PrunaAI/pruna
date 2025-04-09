789898ff2e3691105ae058b3ae64458227c39ca0
Begum Cig <begumcig@gmail.com>
2025-04-09 14:30:17 +0000

    fix: support older python versions

--- src/pruna/data/pruna_datamodule.py
+++ src/pruna/data/pruna_datamodule.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import inspect
 from functools import partial
 from typing import Callable, List, Tuple, Union
