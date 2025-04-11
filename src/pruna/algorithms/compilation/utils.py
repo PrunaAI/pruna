183b81deb12d6eb8aa066ccb19b25141005b193c
llcnt <louis.leconte@ens-paris-saclay.fr>
2025-04-11 08:44:41 +0000

    fix: add fa to the new created utils after rebasing

--- src/pruna/algorithms/compilation/utils.py
+++ src/pruna/algorithms/compilation/utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # This script is inspired from https://github.com/chu-tianxiang/QuIP-for-all
+from __future__ import annotations
 
 from typing import Optional
 
