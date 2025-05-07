4cd7b1c7f84d9226f613d9b86f6202817931fb23
nifleisch <nils.fleischmann@pruna.ai>
2025-05-07 09:43:15 +0000

    test: add integration test for FORA

--- tests/algorithms/testers/caching.py
+++ tests/algorithms/testers/caching.py
@@ -1,5 +1,6 @@
 from pruna import PrunaModel
 from pruna.algorithms.caching.deepcache import DeepCacheCacher
+from pruna.algorithms.caching.fora import FORACacher
 
 from .base_tester import AlgorithmTesterBase
 
@@ -15,3 +16,16 @@ class TestDeepCache(AlgorithmTesterBase):
     def post_smash_hook(self, model: PrunaModel) -> None:
         """Hook to modify the model after smashing."""
         assert hasattr(model, "deepcache_unet_helper")
+
+
+class TestFORA(AlgorithmTesterBase):
+    """Test the fora algorithm."""
+
+    models = ["flux_tiny_random"]
+    reject_models = ["opt_125m"]
+    allow_pickle_files = False
+    algorithm_class = FORACacher
+
+    def post_smash_hook(self, model: PrunaModel) -> None:
+        """Hook to modify the model after smashing."""
+        assert hasattr(model, "cache_helper")
