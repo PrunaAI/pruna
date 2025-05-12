74c010d9cc2b23b7d4c2c111526cca688daff0a8
llcnt <louis.leconte@ens-paris-saclay.fr>
2025-05-12 14:27:45 +0000

    feat: continue changing fuser name to factorizer for cache and compile

--- src/pruna/algorithms/compilation/torch_compile.py
+++ src/pruna/algorithms/compilation/torch_compile.py
@@ -50,7 +50,7 @@ class TorchCompileCompiler(PrunaCompiler):
     run_on_cuda = True
     dataset_required = False
     compatible_algorithms = dict(
-        fuser=["qkv_fusing"],
+        factorizer=["qkv_factorizing"],
         quantizer=["half", "hqq_diffusers", "diffusers_int8", "gptq", "llm_int8", "hqq"],
         cacher=["deepcache", "fora"],
     )
