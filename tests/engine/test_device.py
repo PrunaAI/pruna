2e6a072bd4c90bb1da7ba76a4f6c3da63866cba7
johannaSommer <johanna@mail-sommer.com>
2025-05-16 13:38:46 +0000

    test: add tests for device casting functionality

--- tests/engine/test_device.py
+++ tests/engine/test_device.py
@@ -1,7 +1,8 @@
 import pytest
 import torch
 from pruna.config.smash_config import SmashConfig
-
+from pruna.engine.utils import move_to_device, get_device
+from diffusers import StableDiffusionPipeline
 DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
 @pytest.mark.cuda
@@ -39,3 +40,35 @@ def test_device_available(device: str | torch.device, expected: str) -> None:
     smash_config = SmashConfig(device=device)
     assert smash_config.device == expected
 
+
+@pytest.mark.cuda
+@pytest.mark.parametrize(
+    "input_device,target_device",
+    [
+        ("cpu", "cuda"),
+        ("cuda", "cpu"),
+    ],
+)
+def test_device_casting(input_device: str | torch.device, target_device: str | torch.device) -> None:
+    """Test that the device can be cast to the target device."""
+    model = torch.nn.Linear(10, 10)
+    model.to(input_device)
+    move_to_device(model, target_device)
+    assert get_device(model) == target_device
+
+
+@pytest.mark.parametrize(
+    "target_device",
+    [
+        "cuda",
+        "cpu",
+    ],
+)
+def test_accelerate_to_single_casting(target_device: str | torch.device) -> None:
+    """Test that the device can be cast to the target device."""
+    model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", device_map="balanced")
+    device_map = model.hf_device_map
+    move_to_device(model, target_device)
+    assert get_device(model) == target_device 
+    move_to_device(model, "accelerate", device_map=device_map)
+    assert get_device(model) == "accelerate" 
