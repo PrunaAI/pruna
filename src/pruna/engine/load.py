1758f861444f1fa3f4003857f74a5641796a3720
llcnt <louis.leconte@ens-paris-saclay.fr>
2025-05-22 16:28:04 +0000

    feat: manage reloading errors

--- src/pruna/engine/load.py
+++ src/pruna/engine/load.py
@@ -340,16 +340,27 @@ def load_hqq(model_path: str, **kwargs) -> Any:
             " --extra-index-url https://prunaai.pythonanywhere.com/`."
         )
         raise
+    if os.path.exists(os.path.join(model_path, "hqq_language_model")):
+        q_path = os.path.join(model_path, "hqq_language_model")
+    else:
+        q_path = model_path
 
     try:  # Try to use pipeline for HF specific HQQ quantization
-        model = HQQModelForCausalLM.from_quantized(
-            model_path, **filter_load_kwargs(HQQModelForCausalLM.from_quantized, kwargs)
+        qmodel = HQQModelForCausalLM.from_quantized(
+            q_path, **filter_load_kwargs(HQQModelForCausalLM.from_quantized, kwargs)
         )
     except Exception as e:  # Default to generic HQQ pipeline if it fails
         pruna_logger.error(f"Error loading model using HQQ: {e}")
-        model = AutoHQQHFModel.from_quantized(model_path, **filter_load_kwargs(AutoHQQHFModel.from_quantized, kwargs))
+        qmodel = AutoHQQHFModel.from_quantized(q_path, **filter_load_kwargs(AutoHQQHFModel.from_quantized, kwargs))
 
-    return model
+    original_config = load_json_config(model_path, "config.json")
+    if original_config["architectures"][0] == "JanusForConditionalGeneration":
+        cls = getattr(transformers, "JanusForConditionalGeneration")
+        model = cls.from_pretrained(model_path, **kwargs)
+        model.model.language_model = qmodel
+        return model
+    else:
+        return qmodel
 
 
 def load_torch_artifacts(model_path: str, **kwargs) -> None:
