cd86a26cb644d53874a912ea86d103da47be2de7
Begum Cig <begumcig@gmail.com>
2025-07-16 13:33:56 +0000

    feat:clipiqa metric

--- tests/evaluation/test_torch_metrics.py
+++ tests/evaluation/test_torch_metrics.py
@@ -52,6 +52,16 @@ def test_clip_score(dataloader_fixture: Any) -> None:
     score = metric.compute()
     assert score.result > 0.0 and score.result < 100.0
 
+@pytest.mark.cpu
+@pytest.mark.parametrize("dataloader_fixture", ["LAION256"], indirect=True)
+def test_clipiqa(dataloader_fixture: Any) -> None:
+    """Test the clipiqa."""
+    metric = TorchMetricWrapper("clipiqa")
+    x, gt = next(iter(dataloader_fixture))
+    metric.update(x, gt, gt)
+    score = metric.compute()
+    assert score.result > 0.0 and score.result < 1.0
+
 
 @pytest.mark.cpu
 @pytest.mark.parametrize("dataloader_fixture", ["ImageNet"], indirect=True)
