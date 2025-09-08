1b8f0035430d1ac79da6501f527d0bd5062c2668
Nils Fleischmann <nils.fleischmann@pruna.ai>
2025-09-08 12:05:59 +0000

    refactor: use seperate collate function for prompt only datasets

--- src/pruna/data/collate.py
+++ src/pruna/data/collate.py
@@ -54,9 +54,7 @@ def image_format_to_transforms(output_format: str, img_size: int) -> transforms.
         raise ValueError(f"Invalid output format: {output_format}")
 
 
-def image_generation_collate(
-    data: Any, img_size: int | None, output_format: str = "int"
-) -> Tuple[List[str], torch.Tensor]:
+def image_generation_collate(data: Any, img_size: int, output_format: str = "int") -> Tuple[List[str], torch.Tensor]:
     """
     Custom collation function for text-to-image generation datasets.
 
@@ -67,8 +65,8 @@ def image_generation_collate(
     ----------
     data : Any
         The data to collate.
-    img_size : int | None
-        The size of the image to resize to. None if dataset does not contain images.
+    img_size : int
+        The size of the image to resize to.
     output_format : str
         The output format, in ["int", "float", "normalized"].
         With "int", output tensors have integer values between 0 and 255. With "float", they have float values
@@ -79,23 +77,38 @@ def image_generation_collate(
     Tuple[torch.Tensor, Any]
         The collated data with size img_size and normalized to [0, 1].
     """
-    if img_size is not None:
-        transformations = image_format_to_transforms(output_format, img_size)
+    transformations = image_format_to_transforms(output_format, img_size)
     images, texts = [], []
 
     for item in data:
-        texts.append(item["text"])
-        if img_size is None:
-            continue
         image = item["image"]
         if image.mode != "RGB":
             image = image.convert("RGB")
         image_tensor = transformations(image)
         images.append(image_tensor)
-    if img_size is None:
-        return texts, None
+        texts.append(item["text"])
+
     images_tensor = torch.stack(images)
     return texts, images_tensor
+
+
+def prompt_collate(data: Any) -> Tuple[List[str], None]:
+    """
+    Custom collation function for prompt datasets.
+
+    Expects a ``text`` column containing the clear-text prompt in the dataset.
+
+    Parameters
+    ----------
+    data : Any
+        The data to collate.
+
+    Returns
+    -------
+    Tuple[List[str], None]
+        The collated data.
+    """
+    return [item["text"] for item in data], None
 
 
 def audio_collate(data: Any) -> Tuple[List[str], List[str]]:
@@ -232,4 +245,5 @@ pruna_collate_fns: dict[str, Callable] = {
     "image_classification_collate": image_classification_collate,
     "text_generation_collate": text_generation_collate,
     "question_answering_collate": question_answering_collate,
+    "prompt_collate": prompt_collate,
 }
