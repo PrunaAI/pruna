40504c63563814759112f19a3ac23a7a7141aeb1
Begum Cig <begumcig@gmail.com>
2025-10-24 10:09:15 +0000

    test: add more dtype tests for artifact saver

--- tests/evaluation/test_artifactsaver.py
+++ tests/evaluation/test_artifactsaver.py
@@ -9,7 +9,6 @@ from pruna.evaluation.artifactsavers.video_artifactsaver import VideoArtifactSav
 from pruna.evaluation.artifactsavers.utils import assign_artifact_saver
 from pruna.evaluation.metrics.vbench_utils import load_video
 from PIL import Image
-import itertools
 
 
 def test_create_alias():
@@ -43,22 +42,49 @@ def test_assign_artifact_saver_invalid():
         assign_artifact_saver("text")
 
 @pytest.mark.parametrize(
-    "export_format, save_from_type",
-    list(itertools.product(["gif", "mp4"], ["np", "pt", "pil"]))
+    "export_format, save_from_type, save_from_dtype",
+    [pytest.param("gif", "np", "uint8"),
+    pytest.param("gif", "np", "float32"),
+    # Numpy doesn't have half precision, so we do not test for float16
+    pytest.param("gif", "pt", "float32"),
+    pytest.param("gif", "pt", "float16"),
+    pytest.param("gif", "pt", "uint8"),
+    # PIL doesnot support creating images from float numpy arrays, so we only test uint8.
+    pytest.param("gif", "pil", "uint8"),
+    pytest.param("mp4", "np", "uint8"),
+    pytest.param("mp4", "np", "float32"),
+    pytest.param("mp4", "pt", "float32"),
+    pytest.param("mp4", "pt", "float16"),
+    pytest.param("mp4", "pt", "uint8"),
+    # PIL doesnot support creating images from float numpy arrays, so we only test uint8.
+    pytest.param("mp4", "pil", "uint8"),]
 )
-def test_video_artifact_saver_tensor(export_format: str, save_from_type: str):
-    """ Test that we can save a video from numpy, torch and PIL in mp4 and gif formats."""
+def test_video_artifact_saver_tensor(export_format: str, save_from_type: str, save_from_dtype: str):
+    """ Test that we can save a video from numpy, torch and PIL in mp4 and gif formats. """
     with tempfile.TemporaryDirectory() as tmp_path:
         saver = VideoArtifactSaver(root=tmp_path, export_format=export_format)
         # create a fake video:
         if save_from_type == "pt":
-            dummy_video = torch.randint(0, 255, (2, 3, 16, 16), dtype=torch.uint8)
+            # Unfortunately, neither torch nor numpy have one random generator function that can support all dtypes.
+            # Therefore, we need to use different functions for int and float dtypes.
+            if save_from_dtype == "uint8":
+                dtype = getattr(torch, save_from_dtype)
+                dummy_video = torch.randint(0, 256, (2, 3, 16, 16), dtype=dtype)
+            else:
+                dtype = getattr(torch, save_from_dtype)
+                dummy_video = torch.randn(2, 3, 16, 16, dtype=dtype)
         elif save_from_type == "np":
-            dummy_video = np.random.randint(0, 255, (2, 16, 16, 3), dtype=np.uint8)
+            if save_from_dtype == "uint8":
+                dtype = getattr(np, save_from_dtype)
+                dummy_video = np.random.randint(0, 256, (2, 16, 16, 3), dtype=dtype)
+            else:
+                rng = np.random.default_rng()
+                dtype = getattr(np, save_from_dtype)
+                dummy_video = rng.random((2, 16, 16, 3), dtype=dtype)
         elif save_from_type == "pil":
-            dummy_video = np.random.randint(0, 255, (2, 16, 16, 3), dtype=np.uint8)
-            dummy_video = [Image.fromarray(frame) for frame in dummy_video]
-
+            dtype = getattr(np, save_from_dtype)
+            dummy_video = np.random.randint(0, 256, (2, 16, 16, 3), dtype=dtype)
+            dummy_video = [Image.fromarray(frame.astype(np.uint8)) for frame in dummy_video]
         path = saver.save_artifact(dummy_video)
         assert path.exists()
         assert path.suffix == f".{export_format}"
