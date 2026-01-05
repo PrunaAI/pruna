8215794f0f3abdaf4890de6b1f13086fbe85e63f
Ubuntu <ubuntu@ip-172-31-16-188.eu-north-1.compute.internal>
2026-01-05 09:08:32 +0000

    added PERP recovery algorithms without distillion

--- src/pruna/algorithms/base/tags.py
+++ src/pruna/algorithms/base/tags.py
@@ -64,6 +64,10 @@ class AlgorithmTag(Enum):
         "batcher",
         "Batching groups multiple inputs together to be processed simultaneously, improving computational efficiency and reducing overall processing time.",
     )
+    RECOVERER = (
+        "recoverer",
+        "Recovery restores the performance of a model after compression.",
+    )
 
     def __init__(self, name: str, description: str):
         """
