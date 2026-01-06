3e5d9c844b7c31201f3b405fb5cb3b736f6fc9e3
Ubuntu <ubuntu@ip-172-31-16-188.eu-north-1.compute.internal>
2026-01-06 13:23:36 +0000

    adding revocery algorithms  with distillation

--- src/pruna/algorithms/perp.py
+++ src/pruna/algorithms/perp.py
@@ -1,13 +1,26 @@
+# Copyright 2025 - Pruna AI GmbH. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import annotations
 
 from typing import Iterable
 
 from pruna.algorithms.base.tags import AlgorithmTag
-
 from pruna.algorithms.global_utils.recovery.perp_recoverer import PERPRecoverer
 
 
-class TextToImagePERP(PERPRecoverer): 
+class TextToImagePERP(PERPRecoverer):
     """
     PERP recoverer for text-to-image models.
 
@@ -97,7 +110,7 @@ class TextToTextInPlacePERP(TextToTextPERP):
         super().__init__(use_lora=False)
 
 
-class TextToTextLoRA(TextToTextPERP): 
+class TextToTextLoRA(TextToTextPERP):
     """
     LoRA recoverer for text-to-text models.
 
