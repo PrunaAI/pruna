a06ea400b1814db1f3a65099610c6ee411a95186
davidberenstein1957 <david.m.berenstein@gmail.com>
2026-02-26 12:08:03 +0100

    feat: enhance dataset definitions and sampling functionality
    - Added comments to `base_datasets` for clarity on dataset sizes for TinyCIFAR10, TinyMNIST, and TinyImageNet.
    - Updated `setup_parti_prompts_dataset` to include `fraction`, `train_sample_size`, and `test_sample_size` parameters for improved dataset sampling control.
    - Integrated `define_sample_size_for_dataset` and `stratify_dataset` functions to manage dataset size and stratification.
    
    These changes improve the usability and documentation of dataset configurations.

--- src/pruna/data/__init__.py
+++ src/pruna/data/__init__.py
@@ -78,16 +78,19 @@ base_datasets: dict[str, Tuple[Callable, str, dict[str, Any]]] = {
         "image_classification_collate",
         {"img_size": 32},
     ),
+    # our full CIFAR10 has 50k train and 10k test
     "TinyCIFAR10": (
         partial(setup_cifar10_dataset, train_sample_size=800, test_sample_size=100),
         "image_classification_collate",
         {"img_size": 32},
     ),
+    #  our full MNIST has 60k train and 10k test
     "TinyMNIST": (
         partial(setup_mnist_dataset, train_sample_size=800, test_sample_size=100),
         "image_classification_collate",
         {"img_size": 28},
     ),
+    # our full ImageNet has 100k train and 10k val
     "TinyImageNet": (
         partial(setup_imagenet_dataset, train_sample_size=1000, test_sample_size=100),
         "image_classification_collate",
