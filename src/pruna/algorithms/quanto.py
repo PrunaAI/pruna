13ba4b0e1eb2b341ddba6730224f7e6746e55e08
Gaspar Rochette <gaspar.rochette@pruna.ai>
2026-06-19 15:51:54 +0000

    fix: switch quantop saving to reapply for transformers 5 compatilibity

--- src/pruna/algorithms/quanto.py
+++ src/pruna/algorithms/quanto.py
@@ -44,7 +44,7 @@ class Quanto(PrunaAlgorithmBase):
     algorithm_name: str = "quanto"
     group_tags: list[tags] = [tags.QUANTIZER]
     references: dict[str, str] = {"GitHub": "https://github.com/huggingface/optimum-quanto"}
-    save_fn: SAVE_FUNCTIONS = SAVE_FUNCTIONS.pickled
+    save_fn: SAVE_FUNCTIONS = SAVE_FUNCTIONS.reapply
     tokenizer_required: bool = False
     processor_required: bool = False
     dataset_required: bool = False
