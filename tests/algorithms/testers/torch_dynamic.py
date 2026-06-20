d4d802c50907139d0cb2165b5e768cfe695f148e
Gaspar Rochette <gaspar.rochette@pruna.ai>
2026-06-20 11:10:08 +0000

    test: add smollm-135 test for torch dynamic to prove fix is effective

--- tests/algorithms/testers/torch_dynamic.py
+++ tests/algorithms/testers/torch_dynamic.py
@@ -6,7 +6,7 @@ from .base_tester import AlgorithmTesterBase
 class TestTorchDynamic(AlgorithmTesterBase):
     """Test the torch dynamic quantizer."""
 
-    models = ["shufflenet"]
+    models = ["shufflenet", "smollm_135m"]
     reject_models = []
     allow_pickle_files = False
     algorithm_class = TorchDynamic
