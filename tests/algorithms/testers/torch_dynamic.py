0e4d1cf86b64f2666bfbd8c26460a3d08fce26d9
Begüm Çığ <begumcig@gmail.com>
2026-06-22 12:11:47 +0200

    test: fix the model dtype for non cpu supported type (#698)

--- tests/algorithms/testers/torch_dynamic.py
+++ tests/algorithms/testers/torch_dynamic.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from pruna.algorithms.torch_dynamic import TorchDynamic
+from pruna.engine.utils import get_device_type
 
 from .base_tester import AlgorithmTesterBase
 
@@ -11,3 +14,8 @@ class TestTorchDynamic(AlgorithmTesterBase):
     allow_pickle_files = False
     algorithm_class = TorchDynamic
     metrics = ["latency"]
+
+    def pre_smash_hook(self, model: Any) -> None:
+        """Ensure CPU dynamic quantized linear layers receive float32 activations."""
+        if get_device_type(model) == "cpu":
+            model.float()
